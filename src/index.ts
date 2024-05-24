54baca08c539c4e5c7f48b51baaac7b4fd693b1e
daehyeong2 <baconbacon1231@gmail.com>
2024-05-24 22:23:38 +0900

    Visit Counter (20.0~20.5)

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,62 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	DB: KVNamespace;
+}
+
+// @ts-ignore
+import home from './home.html';
+import { makeBadge } from './utils';
+
+function handleHome() {
+	return new Response(home, {
+		headers: {
+			'Content-Type': 'text/html;chartset=utf-8',
+		},
+	});
+}
+
+async function handleVisit(searchParams: URLSearchParams, env: Env) {
+	const page = searchParams.get('page');
+	if (!page) return handleBadRequest();
+	const kvPage = await env.DB.get(page);
+	let value = 1;
+	if (!kvPage) {
+		await env.DB.put(page, value + '');
+	} else {
+		value = parseInt(kvPage) + 1;
+		await env.DB.put(page, value + '');
+	}
+	return new Response(makeBadge(value), {
+		headers: {
+			'Content-Type': 'image/svg+xml;charset=utf-8',
+		},
+	});
+}
+
+function handleBadRequest() {
+	return new Response(null, {
+		status: 400,
+	});
+}
+
+function handleNotFound() {
+	return new Response(null, {
+		status: 404,
+	});
+}
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const { pathname, searchParams } = new URL(request.url);
+		switch (pathname) {
+			case '/':
+				return handleHome();
+			case '/visit':
+				return handleVisit(searchParams, env);
+			default:
+				return handleNotFound();
+		}
 	},
 };
